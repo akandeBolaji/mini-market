6bfc48371905650220546c25a8fc158d662fb175
akandeBolaji <akandbolajidanielofficial@gmail.com>
2020-05-19 01:20:24 +0100

    chore: set up db migrations, models and seeds

---
+++ app/Models/Market.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Market extends Model
+{
+    protected $fillable = [
+        'name',
+        'catgeory_id',
+        'address_address',
+        'address_latitude',
+        'address_longitude'
+    ];
+
+    public function images()
+	{
+		return $this->hasMany(Image::class);
+    }
+
+    public function category()
+	{
+		return $this->belongsTo(Category::class);
+    }
+}
---
+++ database/migrations/2020_05_18_234452_create_markets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMarketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('markets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('category_id')->constrained('food_categories');
+            $table->string('address_address')->nullable();
+            $table->double('address_latitude')->nullable();
+            $table->double('address_longitude')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('markets');
+    }
+}
