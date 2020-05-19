4e93b8b0cc00a3267dd86f627e2a43d12260645d
akandeBolaji <akandbolajidanielofficial@gmail.com>
2020-05-19 02:34:06 +0100

    feature: setup frontend admin auth

---
+++ resources/js/auth.js
@@ -0,0 +1,17 @@
+import bearer from '@websanova/vue-auth/drivers/auth/bearer'
+import axios from '@websanova/vue-auth/drivers/http/axios.1.x'
+import router from '@websanova/vue-auth/drivers/router/vue-router.2.x'
+// Auth base configuration some of this options
+// can be override in method calls
+const config = {
+  auth: bearer,
+  http: axios,
+  router: router,
+  tokenDefaultName: 'mini-market',
+  tokenStore: ['localStorage'],
+  rolesVar: 'role',
+  loginData: {url: 'admin/login', method: 'POST', redirect: '', fetchUser: true},
+  logoutData: {url: 'admin/logout', method: 'POST', redirect: '/', makeRequest: true},
+  refreshData: {url: 'admin/refresh', method: 'GET', enabled: true, interval: 30}
+}
+export default config
---
+++ resources/js/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="main">
+        <header id="header">
+            <h1>
+                Mini Market
+            </h1>
+        </header>
+        <div id="content">
+            Search
+        </div>
+    </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        //
+      }
+    },
+    components: {
+        //
+    }
+  }
+</script>
