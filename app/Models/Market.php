bb5ca5bdd520a22ab9440cd1f6298f3ee4cb3d4d
akandeBolaji <akandbolajidanielofficial@gmail.com>
2020-05-25 01:43:27 +0100

    bug: fix search error by location

--- app/Models/Market.php
+++ app/Models/Market.php
@@ -30,7 +30,7 @@ class Market extends Model
     public static function getNearBy($lat, $lng, $distance = 500, $distanceIn = 'km')
     {
     if ($distanceIn == 'km') {
-    $results = self::with('images')->select(['*', DB::raw('( 0.621371 * 3959 * acos( cos( radians('.$lat.') ) * cos( radians( address_latitude ) ) * cos( radians( address_longitude ) - radians('.$lng.') ) + sin( radians('.$lat.') ) * sin( radians(address_latitude) ) ) ) AS distance')])->havingRaw('distance < '.$distance)->orderBy("distance")->get();
+    $results = self::select(['*', DB::raw('( 0.621371 * 3959 * acos( cos( radians('.$lat.') ) * cos( radians( address_latitude ) ) * cos( radians( address_longitude ) - radians('.$lng.') ) + sin( radians('.$lat.') ) * sin( radians(address_latitude) ) ) ) AS distance')])->havingRaw('distance < '.$distance)->orderBy("distance")->with('images')->get();
     } else {
     $results = self::select(['*', DB::raw('( 3959 * acos( cos( radians('.$lat.') ) * cos( radians( lat ) ) * cos( radians( lng ) - radians('.$lng.') ) + sin( radians('.$lat.') ) * sin( radians(lat) ) ) ) AS distance')])->havingRaw('distance < '.$distance)->get();
     }
